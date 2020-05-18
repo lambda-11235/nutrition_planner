f324395cb34662ef956d730f41846904a2a69714
Taran Lynn <taranlynn0@gmail.com>
2020-05-17 21:49:00 -0700

    Add untracked files

---
+++ gen_blank_data.py
@@ -0,0 +1,18 @@
+
+import argparse
+import glob
+import json
+
+from data import *
+
+parser = argparse.ArgumentParser(
+    description="generate a blank ingredient template")
+parser.add_argument('name', type=str,
+    help="")
+parser.add_argument('--json-indent', type=int, default=2,
+    help="indent on JSON blocks (default %(default)s)")
+args = parser.parse_args()
+
+d = Data()
+d.name = args.name
+print(json.dumps(d.encode(), indent=args.json_indent))
